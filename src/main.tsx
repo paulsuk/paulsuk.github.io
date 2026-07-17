c1ac0a1f5b13abe8b51fb773358d85fa906dfd6a
paulsuk <paulsuk@gatech.edu>
2026-07-17 10:27:42 -0400

    feat: editorial design tokens + base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// @ts-expect-error - fontsource packages don't have TypeScript declarations
+import "@fontsource-variable/fraunces";
+// @ts-expect-error - fontsource packages don't have TypeScript declarations
+import "@fontsource-variable/inter";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
